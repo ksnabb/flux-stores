3cbd0f38224c1eb496b824b2233699594a6e275f
ksnabb <kristoffer.snabb@geonition.org>
2015-02-28 15:12:42 +0200

    EventEmitter that enables any class extending them to trigger events on listeners

---
+++ src/EventEmitter.js
@@ -0,0 +1,27 @@
+export class EventEmitter {
+
+  constructor() {
+    this.listeners = new Map();
+  }
+
+  on(eventType, listener) {
+    let listeners = this.listeners.get(eventType);
+    if (listeners === undefined) {
+      this.listeners.set(eventType, new Set([listener]));
+    } else {
+      listeners.add(listener);
+      this.listeners.set(eventType, listeners);
+    }
+  }
+
+  off(eventType, listener) {
+    this.listeners.get(eventType).delete(listener);
+  }
+
+  trigger(eventType, ...args) {
+    let listeners = this.listeners.get(eventType);
+    if(listeners) {
+      listeners.forEach(fun => fun.apply(this, args));
+    }
+  }
+}
---
+++ test/EventEmitter.js
@@ -0,0 +1,51 @@
+"use strict";
+var should = require("should");
+var EventEmitter = require("../dist/EventEmitter").EventEmitter;
+
+describe("EventEmitter", function() {
+  var eventEmitter;
+  
+  beforeEach(function() {
+    eventEmitter = new EventEmitter();
+  });
+  
+  describe("on", function() {
+    
+    it("should register a listener to receive events of a certain type", function() {
+      eventEmitter.listeners.has("click").should.be.false;
+      let listener = function() {};
+      eventEmitter.on("click", listener);
+      eventEmitter.listeners.has("click").should.be.true;
+      let listeners = eventEmitter.listeners.get("click");
+      listeners.forEach(function(fun) {
+        fun.should.equal(listener);
+      });
+    })
+  });
+  
+  describe("off", function() {
+    
+    it("should remove a listener from the EventEmitter", function() {
+      let listener = function() {};
+      eventEmitter.on("click", listener);
+      eventEmitter.listeners.has("click").should.be.true;
+      eventEmitter.off("click", listener);
+      eventEmitter.listeners.get("click").size.should.equal(0);
+    });
+    
+  });
+  
+  describe("trigger", function() {
+    
+    it("should call all functions subscribed for a certain event type", function() {
+      let listener = function(message) {
+        message.should.equal("hello world");
+      }
+      eventEmitter.on("helloWorlds", listener);
+      eventEmitter.trigger("goodbye", "goodbye");
+      eventEmitter.trigger("helloWorlds", "hello world");
+    })
+    
+  });
+  
+});
