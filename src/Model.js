a50902271ee43fe1fe8d1e271a0bf2dfb0cd0be9
ksnabb <kristoffer.snabb@geonition.org>
2015-03-27 22:48:20 +0200

    default values can now be set for a model
    The extend function was developed at the same time to enable actually setting the default values for a model class.

--- src/Model.js
+++ src/Model.js
@@ -3,7 +3,22 @@ import {EventEmitter} from "./EventEmitter";
 export class Model extends EventEmitter {
   constructor(obj = {}) {
     this.arguments = new Map();
+    
+    if(typeof this.defaults === "object") {
+      this.set(this.defaults);
+    } else if (typeof this.defaults === "function") {
+      this.set(this.defaults());
+    }
+    
     this.set(obj);
+  }
+  
+  static extend(obj) {
+    class Child extends this {}
+    for(var funName in obj) {
+      Child.prototype[funName] = obj[funName];
+    }
+    return Child
   }
 
   set(values) {
