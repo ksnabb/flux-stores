11ff490fdfa3f7c2984e427800c45fdb8ee4592f
ksnabb <kristoffer.snabb@geonition.org>
2015-05-16 22:29:14 +0300

    added files to compile and run browser side mocha tests

---
+++ test/browser/main.js
@@ -0,0 +1,6 @@
+/* jshint node: true */
+"use strict";
+require("babel/polyfill");
+var Model = require("../Model");
+var Collection = require("../Collection");
+var EventEmitter = require("../EventEmitter");
